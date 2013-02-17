016d703ccc5b61be6252f6ec07fa805b22ddb99e
James Gill <jamesagill@gmail.com>
2013-02-16 17:20:17 -0800

    Pruning uneeded functions.

--- client/friend-sound.js
+++ client/friend-sound.js
@@ -17,10 +17,6 @@ var player = function () {
   return Players.findOne(Session.get('player_id'));
 };
 
-Template.playlist.current_song = function () {
-  return "Black Steel";
-};
-
 //////
 ////// Initialization
 //////
@@ -36,29 +32,4 @@ Meteor.startup(function () {
     Session.set('player_id', player_id);
   }
 
-  // subscribe to all the players, the game i'm in, and all
-  // the words in that game.
-  Meteor.autorun(function () {
-    Meteor.subscribe('players');
-
-    if (Session.get('player_id')) {
-      var me = player();
-      /*
-      if (me && me.game_id) {
-        Meteor.subscribe('games', me.game_id);
-        Meteor.subscribe('words', me.game_id, Session.get('player_id'));
-      }
-      */
-    }
-  });
-
-  // send keepalives so the server can tell when we go away.
-  //
-  // XXX this is not a great idiom. meteor server does not yet have a
-  // way to expose connection status to user code. Once it does, this
-  // code can go away.
-  //Meteor.setInterval(function() {
-    //if (Meteor.status().connected)
-      //Meteor.call('keepalive', Session.get('player_id'));
-  //}, 20*1000);
 });
